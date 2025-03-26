a5baa074d6ecdb0b8d250992c373e89dc1d25e8b
Hanna Seldén <hanna_lotta@hotmail.com>
2025-03-26 11:51:13 +0100

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header className="App-header">
+		<nav>
+			start
+		</nav>
+	  </header>
+	  <main>
+		<h2>Toys</h2>
+	  </main>
+    </>
+
+  )
+}
+
+export default App
